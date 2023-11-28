5033ccb3704b213613a83258da56e4be7f9fb020
parsamost <138319615+parsamost@users.noreply.github.com>
2023-11-28 21:53:48 +0330

    done

--- assessts/js/script.js
+++ assessts/js/script.js
@@ -5,3 +5,16 @@ navCloseBTN = document.querySelector('.nav-close-btn');
 const navToggleFunc = () => {nav.classList.toggle("active")};
 navMenuBTN.addEventListener('click',navToggleFunc);
 navCloseBTN.addEventListener('click',navToggleFunc);
+
+const themeBtn = document.querySelectorAll('.theme-btn');
+for (let i =0; i < themeBtn.length; i++) {
+  themeBtn[i].addEventListener('click', () => {
+        document.body.classList.toggle('light-theme');
+        document.body.classList.toggle('dark-theme');
+
+        for (let j =0; j < themeBtn.length; j++) {
+          themeBtn[j].classList.toggle('light');
+          themeBtn[j].classList.toggle('dark');
+        }
+    });
+}
